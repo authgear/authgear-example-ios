16e1603018667297b896aa92164cbb0ed281edea
Pius Aboyi <apeapius@gmail.com>
2025-03-11 09:22:58 +0100

    move authgear client app configuration to constants.swift

---
+++ my_demo_app/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  my_demo_app
+//
+//  Created by Pius Aboyi on 11/03/2025.
+//
+enum Constants {
+    static let authgearClientId = "<AUTHGEAR_CLIENT_ID>"
+    static let authgearEndpoint = "<AUTHGEAR_ENDPOINT>"
+    static let authgearRedirectUri = "com.example.authgeardemo://host/path"
+}
